88041391878ab66452007028ac6c40fb742b6d43
Spencer Williams <spencer@aninternetpresence.net>
2018-11-25 14:25:59 -0700

    Stopping due to a major bug
    - The solution for the *Isomorphic* exercise does not pass its own test.
      This discussion is usefu: https://github.com/workshopper/learnyoureact/issues/127
      It looks like react-dom may be deprecated, but if anything node is saying
      that `React.DOM` is null. We're going to try the other react workshop for now.

---
+++ 08_isomorphic/program.js
@@ -0,0 +1,57 @@
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var DOM = React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
+
+var browserify = require('browserify');
+var babelify = require("babelify");
+
+const express = require('express');
+const app = express();
+
+const port = process.argv[2] || 3000;
+
+app.set('port', port);
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
+
+require('babel/register')({
+    ignore: false
+});
+
+var TodoBox = require('./views/index.jsx');
+
+app.use('/bundle.js', function (req, res) {
+    res.setHeader('Content-Type', 'application/javascript');
+
+    browserify("./app.js")
+        .transform("babelify", {presets: ["es2015", "react"]})
+        .bundle()
+        .pipe(res);
+});
+
+app.use('/', function (req, res) {
+    var initialData = JSON.stringify(data);
+    var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+
+    res.setHeader('Content-Type', 'text/html');
+
+    var html = ReactDOMServer.renderToStaticMarkup(body(null,
+        div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+        script({
+            id: 'initial-data',
+            type: 'text/plain',
+            'data-json': initialData
+        }),
+        script({src: '/bundle.js'})
+    ));
+
+    res.end(html);
+});
+
+app.listen(app.get('port'), function() {
+    console.log(`Express server is up on port ${port}`);
+});
